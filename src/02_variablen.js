081f0e4835373075c825bf98e06602503252ebbd
7Math <168723211+7Math@users.noreply.github.com>
2024-05-08 08:04:05 +0000

    1_Commit

---
+++ src/02_variablen.js
@@ -0,0 +1,17 @@
+//  Zeilekommentar | strg + #
+
+/**
+ * 
+ * Block-Kommentar
+ * 
+ * 
+ */
+/***** variabel 1 *******/
+
+// console.log("Hi");
+// console.log(Hi);
+
+/*Deklaration + Wertzuweisung I */
+
+let firstName;
+console.log(firstName);
